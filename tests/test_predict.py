ca685383d1ad8d1771b81ba048228dc2c5b19023
joshmantovani <josh.mantovani@dataprime.ai>
2022-01-15 23:52:16 -0700

    Conforming to pep8

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,5 +1,4 @@
 import json
-import os
 from PIL import Image
 import boto3
 from botocore import UNSIGNED  # contact public s3 buckets anonymously
@@ -8,7 +7,11 @@ from io import BytesIO
 
 from src.resnet_model import ResnetModel
 
-def load_files_from_s3(keys, bucket_name='bird-classification-bucket'):
+
+def load_files_from_s3(
+        keys,
+        bucket_name='bird-classification-bucket'
+        ):
     """Retrieves files anonymously from my public S3 bucket"""
     s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
     s3_files = []
@@ -30,7 +33,13 @@ def test_predict():
         path_to_pretrained_model='models/trained_model_resnet50.pt')
     with open('src/index_to_class_label.json', 'rb') as f:
         index_to_class_labels = json.load(f)
-    index_to_class_labels = {int(k): v for k, v in index_to_class_labels.items()}
+    index_to_class_labels = {
+        int(k): v for k, v in index_to_class_labels.items()}
     img = load_files_from_s3(["train/AFRICAN CROWNED CRANE/001.jpg"])[0]
-    formatted_prediction = model.predict_proba(img, 3, index_to_class_labels, show=False)
+    formatted_prediction = model.predict_proba(
+        img,
+        3,
+        index_to_class_labels,
+        show=False
+        )
     assert formatted_prediction[0][0] == "African Crowned Crane"
